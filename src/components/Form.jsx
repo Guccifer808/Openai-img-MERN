7c62c86e9463a5e76e94581872ef98a8b0a89ca3
G808 <hucciallstars@gmail.com>
2023-02-20 20:53:13 +0200

    Finished with client part for now

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="mb-2 flex items-center gap-2">
+        <label htmlFor={name} className="block text-sm font-medium text-black">
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="rounded-md bg-stone-200 py-1 px-2 text-xs font-semibold text-black"
+          >
+            Surprise Me
+          </button>
+        )}
+      </div>
+      {/* Form input */}
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+        required
+        className="block w-full rounded-lg border border-gray-300 bg-stone-50 p-3 text-sm text-slate-900 outline-none focus:border-[#4649ff] focus:ring-[#4649ff]"
+      />
+    </div>
+  );
 };
 
 export default Form;
--- src/pages/CreateImage.jsx
+++ src/pages/CreateImage.jsx
@@ -12,6 +12,17 @@ const CreateImage = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   // loading
   const [loading, setLoading] = useState(false);
+  // to generate the image > calling backend
+  const generateImg = () => {};
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
   return (
     <section className="mx-auto max-w-7xl">
       <div>
@@ -21,6 +32,75 @@ const CreateImage = () => {
           others
         </p>
       </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <Form
+            labelName="Your name"
+            name="name"
+            type="text"
+            placeholder="John Smith"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <Form
+            labelName="Prompt"
+            name="prompt"
+            type="text"
+            placeholder="A Synthwave Hedgehog, Blade Runner Cyberpunk'"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+          {/* div where image will be shown */}
+          <div className="relative h-64 w-64 items-center justify-center rounded-md border-gray-300 bg-stone-50 p-3 text-sm text-slate-900 focus:border-blue-500 focus:ring-blue-500">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="h-full w-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="h-9/12 w-9/12 object-contain opacity-50"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex items-center justify-center rounded-md bg-[rgba(0,0,0,0.5)]">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        {/* Submit button wrap */}
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImg}
+            className="text-md w-full rounded-md bg-green-700/90 px-5 py-2.5 text-center font-medium text-white sm:w-auto"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        {/* Share */}
+        <div className="mt-10">
+          <p className="mt-2 text-[14px] text-gray-400">
+            If you love this OpenAI generated image, you can share it with
+            others
+          </p>
+          <button
+            type="submit"
+            className="mt-3 w-full bg-[#6469ff] px-5 py-2.5 text-center text-sm font-medium text-white sm:w-auto"
+          >
+            {loading ? "Sharing..." : "Share it with others"}
+          </button>
+        </div>
+      </form>
     </section>
   );
 };
